4d9cc389b2a176c47c06afb72a30b616ff58aa8f
beedrey07 <beedrey07@gmail.com>
2024-06-05 12:59:35 +0200

    Verify email view

--- lib/main.dart
+++ lib/main.dart
@@ -36,18 +36,44 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-            final user = FirebaseAuth.instance.currentUser;
-            if (user?.emailVerified ?? false) {
-              print("You are a verified user");
-            } else {
-              print("You need to verify your email first");
-            }
-              return Text("Done");
+              // final user = FirebaseAuth.instance.currentUser;
+              // print(user);
+              // if (user?.emailVerified ?? false) {
+              //   return const Text("Done");
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return LoginView();
             default:
               return const Text('Loading...');
           }
         },
       ),
+    );
+  }
+}
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifiedEmailViewState();
+}
+
+class _VerifiedEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Text("Please verify your email address:"),
+        TextButton(
+          onPressed: () async {
+            final user = FirebaseAuth.instance.currentUser;
+            await user?.sendEmailVerification();
+          },
+          child: const Text("Send Email Verification"),
+        )
+      ],
     );
   }
 }
