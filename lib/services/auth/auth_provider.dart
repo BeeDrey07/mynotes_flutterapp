276deeb8c95e48e8a4ad37ee38bad059ec78ec86
beedrey07 <beedrey07@gmail.com>
2024-06-06 21:51:20 +0200

    clean up views

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:mynotes/services/auth/auth_user.dart';
 
 abstract class AuthProvider {
+
+  Future<void> initialize();
+
   AuthUser? get currentUser;
   Future<AuthUser> logIn({
     required String email,
